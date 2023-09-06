bc513162232f07afef55028949a91fe0ec40d0ab
Dave Welsh <davewelsh79@gmail.com>
2023-09-06 15:41:32 -0400

    Switched to IHttpClientFactory

--- XMLTest/FourRefuel/Xml/XmlClient.cs
+++ XMLTest/FourRefuel/Xml/XmlClient.cs
@@ -1,14 +1,28 @@
 ﻿namespace XMLTest.FourRefuel.Xml;
 
+using System.Net.Http;
 using System.Xml.Serialization;
 
+/// <inheritdoc/>
 public class XmlClient : IClient
 {
     private const string Endpoint = "https://secure.qccareerschool.com/test.xml";
 
+    private readonly IHttpClientFactory httpClientFactory;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="XmlClient"/> class.
+    /// </summary>
+    /// <param name="httpClientFactory">An instance of an IHttpClientFactory.</param>
+    public XmlClient(IHttpClientFactory httpClientFactory)
+    {
+        this.httpClientFactory = httpClientFactory;
+    }
+
+    /// <inheritdoc/>
     public async Task<IEnumerable<Invoice>> GetAllAsync()
     {
-        using var client = new HttpClient();
+        using var client = this.httpClientFactory.CreateClient();
         using var response = await client.GetAsync(Endpoint);
         if (!response.IsSuccessStatusCode)
         {
@@ -19,7 +33,6 @@ public class XmlClient : IClient
 
         var serializer = new XmlSerializer(typeof(XmlResponse));
         var fourRefuelResponse = (XmlResponse?)serializer.Deserialize(stream);
-
         if (fourRefuelResponse == null)
         {
             throw new Exception("Unable to deserialize");
--- XMLTest/Program.cs
+++ XMLTest/Program.cs
@@ -1,7 +1,40 @@
-﻿var client = new XMLTest.FourRefuel.Xml.XmlClient();
-var invoices = await client.GetAllAsync();
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using XMLTest.FourRefuel;
+using XMLTest.FourRefuel.Xml;
 
-foreach (var invoice in invoices)
+internal class Program
 {
-    Console.WriteLine($"Invoice #{invoice.Id} has date {invoice.Date}");
+    private readonly IClient client;
+
+    private static async Task Main(string[] args)
+    {
+        var builder = Host.CreateDefaultBuilder(args);
+        builder.ConfigureServices((context, services) =>
+        {
+            services.AddHttpClient();
+            services.AddTransient<IClient, XmlClient>();
+            services.AddSingleton<Program>();
+        });
+
+        using var host = builder.Build();
+
+        var program = host.Services.GetRequiredService<Program>();
+        await program.RunAsync();
+    }
+
+    public Program(IClient client)
+    {
+        this.client = client;
+    }
+
+    public async Task RunAsync()
+    {
+        var invoices = await this.client.GetAllAsync();
+
+        foreach (var invoice in invoices)
+        {
+            Console.WriteLine($"Invoice #{invoice.Id} has date {invoice.Date}");
+        }
+    }
 }
